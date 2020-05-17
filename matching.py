103e15b9505292a128383cb727a2affce5a776e9
Cless <316600949@qq.com>
2020-05-17 21:04:26 +0800

    调整配置，及调整格式

--- matching.py
+++ matching.py
@@ -1,50 +1,47 @@
-
 __author__ = 'Threedog'
 __Date__ = '2018/3/12 23:57'
-
 
 result = None
 
 
-def canConnect(x1,y1,x2,y2,r):
-    '''
+def canConnect(x1, y1, x2, y2, r):
+    """
     :两个方块是否可以连通函数
-    '''
+    """
     # 将传入的二维数组赋值给本地全局变量，
     global result
     result = r
     # 如果有一个为0 直接返回False
     if result[x1][y1] == 0 or result[x2][y2] == 0:
         return False
-    if x1 == x2 and y1 == y2 :
+    if x1 == x2 and y1 == y2:
         return False
     if result[x1][y1] != result[x2][y2]:
         return False
     # 先判断横向可连通
-    if horizontalCheck(x1,y1,x2,y2):
+    if horizontalCheck(x1, y1, x2, y2):
         return True
     # 在判断纵向可连通
-    if verticalCheck(x1,y1,x2,y2):
+    if verticalCheck(x1, y1, x2, y2):
         return True
     # 判断一个拐点的可连通情况
-    if turnOnceCheck(x1,y1,x2,y2):
+    if turnOnceCheck(x1, y1, x2, y2):
         return True
     # 判断两个拐点的可连通情况
-    if turnTwiceCheck(x1,y1,x2,y2):
+    if turnTwiceCheck(x1, y1, x2, y2):
         return True
     # 都不可连通，返回false
     return False
 
 
-def horizontalCheck(x1,y1,x2,y2):
-    '''
+def horizontalCheck(x1, y1, x2, y2):
+    """
     :判断水平方向能够连通
-    '''
+    """
 
     global result
     # 判断两个不是同一个方块
     if x1 == x2 and y1 == y2:
-
         return False
     # 判断两个的纵坐标相同
     if x1 != x2:
@@ -55,16 +52,16 @@ def horizontalCheck(x1,y1,x2,y2):
     if (endY - startY) == 1:
         return True
     # 判断两个方块通路上是否都是0，有一个不是，就说明不能联通，返回false
-    for i in range(startY+1,endY):
+    for i in range(startY + 1, endY):
         if result[x1][i] != 0:
             return False
     return True
 
 
-def verticalCheck(x1,y1,x2,y2):
-    '''
+def verticalCheck(x1, y1, x2, y2):
+    """
     :判断垂直方向能否连通
-    '''
+    """
     global result
     # 判断不是同一个方块
     if x1 == x2 and y1 == y2:
@@ -78,16 +75,16 @@ def verticalCheck(x1,y1,x2,y2):
     if (endX - startX) == 1:
         return True
     # 判断两方块儿通路上是否可连。
-    for i in range(startX+1,endX):
+    for i in range(startX + 1, endX):
         if result[i][y1] != 0:
             return False
     return True
 
 
-def turnOnceCheck(x1,y1,x2,y2):
-    '''
+def turnOnceCheck(x1, y1, x2, y2):
+    """
     :判断单拐点情况能否连通
-    '''
+    """
     global result
     # 实现单拐点校验。
     if x1 == x2 and y1 == y2:
@@ -109,16 +106,16 @@ def turnOnceCheck(x1,y1,x2,y2):
     return False
 
 
-def turnTwiceCheck(x1,y1,x2,y2):
-    '''
+def turnTwiceCheck(x1, y1, x2, y2):
+    """
     :两个拐点的情况能否连通
-    '''
+    """
     global result
     if x1 == x2 and y1 == y2:
         return False
     # 遍历整个数组找合适的拐点
-    for i in range(0,len(result)):
-        for j in range(0,len(result[1])):
+    for i in range(0, len(result)):
+        for j in range(0, len(result[1])):
             # 不为空不能作为拐点
             if result[i][j] != 0:
                 continue
